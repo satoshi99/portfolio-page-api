8e1b3e2240ed22332eec2d85e1134419a710bb1b
satoshi <3104nit@gmail.com>
2022-02-02 10:26:18 +0900

    fix: post_tag_map table

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -1,4 +1,4 @@
 from database import Base
 
 from .admin import Admin
-from .post import Tag, Post, TagPostMapTable
+from .post import Tag, Post
--- src/app/models/post.py
+++ src/app/models/post.py
@@ -1,5 +1,5 @@
 from uuid import uuid4
-from sqlalchemy import Column, String, Boolean, ForeignKey
+from sqlalchemy import Column, String, Boolean, ForeignKey, Table
 from sqlalchemy.dialects.mysql import MEDIUMTEXT
 from sqlalchemy.orm import relationship
 from sqlalchemy_utils import UUIDType
@@ -8,11 +8,12 @@ from .mixins import TimestampMixin
 from database import Base
 
 
-class TagPostMapTable(Base):
-    __tablename__ = "tag_post_map"
-
-    tag_id = Column(UUIDType(binary=False), ForeignKey("tags.id"), primary_key=True)
-    post_id = Column(UUIDType(binary=False), ForeignKey("posts.id"), primary_key=True)
+tag_post_map_table = Table(
+    "tag_post_map",
+    Base.metadata,
+    Column("tag_id", UUIDType(binary=False), ForeignKey("tags.id")),
+    Column("post_id", UUIDType(binary=False), ForeignKey("posts.id"))
+)
 
 
 class Tag(Base, TimestampMixin):
@@ -21,7 +22,7 @@ class Tag(Base, TimestampMixin):
     id = Column(UUIDType(binary=False), primary_key=True, default=uuid4)
     title = Column(String(50), nullable=False, unique=True)
     slug = Column(String(50), nullable=True, unique=True)
-    posts = relationship("Post", secondary=TagPostMapTable, back_populates="tag")
+    posts = relationship("Post", secondary=tag_post_map_table, back_populates="tags")
 
 
 class Post(Base, TimestampMixin):
@@ -34,4 +35,4 @@ class Post(Base, TimestampMixin):
     content = Column(MEDIUMTEXT, nullable=False)
     is_public = Column(Boolean, default=False, nullable=False)
     author_id = Column(UUIDType(binary=False), ForeignKey("admins.id", ondelete="CASCADE"), nullable=True)
-    tags = relationship("Tag", secondary=TagPostMapTable, back_populates="post")
+    tags = relationship("Tag", secondary=tag_post_map_table, back_populates="posts")

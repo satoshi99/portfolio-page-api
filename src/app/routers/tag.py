5462a90fb357b615f024235ed409c5c9d2f0a1a4
satoshi <3104nit@gmail.com>
2022-02-04 23:44:40 +0900

    add: tag router with all crud

---
+++ src/app/routers/tag.py
@@ -0,0 +1,50 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from uuid import UUID
+from typing import List, Union
+from schemas import tag as tag_schema
+from schemas.common import ResponseMsg
+from cruds.tag import TagCrud
+from database import get_db
+
+router = APIRouter(prefix="/tags")
+crud = TagCrud()
+
+
+@router.get("", response_model=Union[List[tag_schema.Tag], ResponseMsg])
+async def get_tags(db: Session = Depends(get_db)):
+    tags = crud.get_tags(db)
+    if not tags:
+        return {"message": "No registered tags"}
+
+    return tags
+
+
+@router.get("/{tag_id}", response_model=tag_schema.Tag)
+async def get_tag(tag_id: UUID, db: Session = Depends(get_db)):
+    tag = crud.get_tag(tag_id, db)
+    if not tag:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Tag not found"
+        )
+    return tag
+
+
+@router.post("/create", response_model=tag_schema.Tag)
+async def create_tag(new_tag: tag_schema.TagCreate, db: Session = Depends(get_db)):
+    return crud.create_tag(new_tag, db)
+
+
+@router.put("/{tag_id}", response_model=tag_schema.Tag)
+async def update_post(tag_id: UUID, new_tag: tag_schema.TagUpdate, db: Session = Depends(get_db)):
+    return crud.update_tag(tag_id, new_tag, db)
+
+
+@router.delete("{tag_id}", response_model=ResponseMsg)
+async def delete_post(tag_id: UUID, db: Session = Depends(get_db)):
+    result = crud.delete_tag(tag_id, db)
+    if result:
+        return {"message": "Successfully Tag Deleted"}
+    else:
+        return {"message": "Failed delete tag object"}

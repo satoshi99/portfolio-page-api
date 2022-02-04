e2c2e983ad310e72c25b7aff92ef87333f5c8e21
satoshi <3104nit@gmail.com>
2022-02-04 23:46:42 +0900

    fix: authentication with jwt

--- src/app/auth.py
+++ src/app/auth.py
@@ -1,21 +1,17 @@
 from datetime import datetime, timedelta
 import uuid
 from uuid import UUID
-from typing import Union
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import HTTPException, status
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from fastapi_csrf_protect import CsrfProtect
 
-from cruds.admin import AdminCRUD
-from schemas import admin as admin_schema
-from schemas import auth as auth_schema
+from cruds.admin import AdminCrud
 from env import CSRF_SECRET_KEY, JWT_SECRET_KEY, JWT_EXPIRE_MINUTES, JWT_NOT_BEFORE_SECONDS, ALGORITHM
 
 
 class AuthJwtCsrf:
-    admin_crud = AdminCRUD()
+    admin_crud = AdminCrud()
     pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
     def verify_password(self, plain_password, hashed_password) -> bool:
@@ -23,14 +19,6 @@ class AuthJwtCsrf:
 
     def get_password_hash(self, password) -> str:
         return self.pwd_context.hash(password)
-
-    def authenticate_admin_user(self, email: str, password: str) -> Union[admin_schema.AdminInDB, bool]:
-        db_admin = self.admin_crud.get_admin_by_email(email)
-        if not db_admin:
-            return False
-        if not self.verify_password(password, db_admin.hashed_password):
-            return False
-        return db_admin
 
     @staticmethod
     def create_access_token(data: dict) -> str:
@@ -61,11 +49,11 @@ class AuthJwtCsrf:
                 detail="Could not validate credentials"
             )
 
-    def verify_and_update_jwt(self, token: str) -> Union[str, bool]:
-        admin_id = self.decode_jwt(token)
-        db_admin = self.admin_crud.get_admin_by_id(admin_id)
-        if not db_admin:
-            return False
-
-        return self.create_access_token({"sub": db_admin.id})
+    # def verify_and_update_jwt(self, token: str) -> Union[str, bool]:
+    #     admin_id = self.decode_jwt(token)
+    #     db_admin = self.admin_crud.get_admin(admin_id)
+    #     if not db_admin:
+    #         return False
+    #
+    #     return self.create_access_token({"sub": db_admin.id})
 

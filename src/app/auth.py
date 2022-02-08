d4c81847741f57b9f083d1f26aee43428080c7cd
satoshi <3104nit@gmail.com>
2022-02-09 01:38:32 +0900

    fix: added update_jwt

--- src/app/auth.py
+++ src/app/auth.py
@@ -1,18 +1,24 @@
 from datetime import datetime, timedelta
 import uuid
 from uuid import UUID
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, Request, Response
+from fastapi.encoders import jsonable_encoder
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from fastapi_csrf_protect import CsrfProtect
 
 from cruds.admin import AdminCrud
-from env import CSRF_SECRET_KEY, JWT_SECRET_KEY, JWT_EXPIRE_MINUTES, JWT_NOT_BEFORE_SECONDS, ALGORITHM
+from env import JWT_SECRET_KEY, JWT_EXPIRE_MINUTES, JWT_NOT_BEFORE_SECONDS, ALGORITHM
 
 
 class AuthJwtCsrf:
     admin_crud = AdminCrud()
     pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    @staticmethod
+    def verify_csrf(request: Request, csrf_protect: CsrfProtect) -> None:
+        csrf_token = csrf_protect.get_csrf_from_headers(request.headers)
+        csrf_protect.validate_csrf(csrf_token)
 
     def verify_password(self, plain_password, hashed_password) -> bool:
         return self.pwd_context.verify(plain_password, hashed_password)
@@ -49,11 +55,11 @@ class AuthJwtCsrf:
                 detail="Could not validate credentials"
             )
 
-    # def verify_and_update_jwt(self, token: str) -> Union[str, bool]:
-    #     admin_id = self.decode_jwt(token)
-    #     db_admin = self.admin_crud.get_admin(admin_id)
-    #     if not db_admin:
-    #         return False
-    #
-    #     return self.create_access_token({"sub": db_admin.id})
-
+    def update_jwt(self, admin_id: UUID, response: Response) -> None:
+        try:
+            new_token = self.create_access_token({"sub": jsonable_encoder(admin_id)})
+            response.set_cookie(
+                key="access_token", value=f"Bearer {new_token}", httponly=True
+            )
+        except Exception as ex:
+            raise ex

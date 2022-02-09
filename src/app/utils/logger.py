13d2c856b43c72071fdfe1cff12513b8d776d979
satoshi <3104nit@gmail.com>
2022-02-09 12:59:49 +0900

    fix: inserted add_filter

--- src/app/utils/logger.py
+++ src/app/utils/logger.py
@@ -1,19 +1,25 @@
 from logging import getLogger, StreamHandler, Formatter, FileHandler, DEBUG
 
 
-def setup_logger(log_folder, modname=__name__):
+def setup_logger(log_folder, log_filter=None, modname=__name__):
     logger = getLogger(modname)
     logger.setLevel(DEBUG)
 
-    sh = StreamHandler()
-    sh.setLevel(DEBUG)
-    formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    sh.setFormatter(formatter)
-    logger.addHandler(sh)
+    if not logger.hasHandlers():
 
-    fh = FileHandler(log_folder)
-    fh.setLevel(DEBUG)
-    fh_formatter = Formatter('%(asctime)s - %(filename)s - %(name)s - %(lineno)d - %(levelname)s - %(message)s')
-    fh.setFormatter(fh_formatter)
-    logger.addHandler(fh)
+        sh = StreamHandler()
+        sh.setLevel(DEBUG)
+        formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        sh.setFormatter(formatter)
+        logger.addHandler(sh)
+
+        fh = FileHandler(log_folder)
+        fh.setLevel(DEBUG)
+        fh_formatter = Formatter('%(asctime)s - %(filename)s - %(name)s - %(lineno)d - %(levelname)s - %(message)s')
+        fh.setFormatter(fh_formatter)
+        logger.addHandler(fh)
+
+        if log_filter:
+            logger.addFilter(log_filter)
+
     return logger

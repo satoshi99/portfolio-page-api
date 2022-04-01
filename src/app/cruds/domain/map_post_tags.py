090af30ec3dd7714ed55d1527a69583ae811fa08
satoshi <3104nit@gmail.com>
2022-04-01 11:43:02 +0900

    fix create and update map with tag_id

--- src/app/cruds/domain/map_post_tags.py
+++ src/app/cruds/domain/map_post_tags.py
@@ -1,3 +1,4 @@
+from uuid import UUID
 from sqlalchemy.orm import Session
 from typing import List
 from schemas import tag as tag_schema
@@ -5,20 +6,21 @@ from models import Post
 from cruds.post import PostCrud
 from cruds.tag import TagCrud
 
+from exceptions import ObjectNotFoundError
+
 
 class MapPostAndTags:
 
     post_crud = PostCrud()
     tag_crud = TagCrud()
 
-    def create_map(self, tags: List[tag_schema.TagCreate], db_post: Post, db: Session) -> Post:
-        for tag in tags:
-            db_tag = self.tag_crud.get_tag_by_title(tag.title, db)
-            if db_tag:
-                db_post.tags.append(db_tag)
-            else:
-                new_tag = self.tag_crud.create_tag(tag, db)
-                db_post.tags.append(new_tag)
+    def create_map(self, tag_ids: List[UUID], db_post: Post, db: Session) -> Post:
+        for tag_id in tag_ids:
+            db_tag = self.tag_crud.get_tag(tag_id, db)
+            if not db_tag:
+                raise ObjectNotFoundError(output_message="The tag was not found by ID")
+
+            db_post.tags.append(db_tag)
 
         db.commit()
         return db_post
@@ -30,17 +32,17 @@ class MapPostAndTags:
         db.commit()
         return db_post
 
-    def update_map(self, new_tags: List[tag_schema.TagCreate], db_post: Post, db: Session) -> Post:
-        db_tags_title = [tag.title for tag in db_post.tags]  # The title list that has already added to map
-        new_tags_title = [tag.title for tag in new_tags]  # The title list that want to create the new map of tags
+    def update_map(self, input_tag_ids: List[UUID], db_post: Post, db: Session) -> Post:
+        db_tag_id_list = [tag.id for tag in db_post.tags]  # The title list that has already added to map
+        input_tag_id_list = [tag_id for tag_id in input_tag_ids]  # The title list that want to create the new map of tags
 
         # tags that except have already added to map
-        new_add_tags = [tag for tag in new_tags if tag.title not in db_tags_title]
-        if new_add_tags:
-            db_post = self.create_map(new_add_tags, db_post, db)
+        new_tag_id_list = [tag_id for tag_id in input_tag_id_list if tag_id not in db_tag_id_list]
+        if new_tag_id_list:
+            db_post = self.create_map(new_tag_id_list, db_post, db)
 
         # tags that have removed from existing map
-        remove_tags = [tag for tag in db_post.tags if tag.title not in new_tags_title]
+        remove_tags = [tag for tag in db_post.tags if tag.id not in input_tag_id_list]
         if remove_tags:
             db_post = self.delete_map(remove_tags, db_post, db)
 
